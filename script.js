300e4c609b7ad2426be0393d30ff278187e9ec34
mrodolfo1981 <60948130+mrodolfo1981@users.noreply.github.com>
2020-11-03 23:14:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    var saudacao = window.document.getElementById('saudacao')
+
     var data = new  Date()
     var hora = data.getHours()
-  
-    msg.innerHTML = `Obrigado pela visita agora São ${hora} horas.`
+    var min = data.getMinutes()
+    var sdc = 'texto'
+
+    msg.innerHTML = `Obrigado pela visita agora São ${hora}  horas e ${min} minutos .`
     
+
     if(hora>= 0 && hora < 12 ){
+        saudacao.innerHTML = `Bom dia!`
         document.body.style.background ='#e2d4b2'
+        
     }else if(hora>=12 && hora<=18){
+        saudacao.innerHTML = `Boa tarde!`
         document.body.style.background ='#b6bad3'
+        
     }else{
+        saudacao.innerHTML = `Boa noite!`
         document.body.style.background ='#3d7395'
+        
     }
 }
 
